fdc6beb7ad03b47d3a62d0b2b24a71f10e5cbded
Junseo Youn <plancatlog@gmail.com>
2017-06-16 20:58:50 +0900

    add network

---
+++ Assets/DayNetworkManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DayNetworkManager : NetworkManager 
+{
+    public virtual void OnClientConnect(NetworkConnection conn)
+    {
+        ClientScene.Ready(conn);
+        ClientScene.AddPlayer(0);
+    }
+}
