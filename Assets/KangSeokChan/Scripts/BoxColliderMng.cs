32ba401f01432f8c1e14c820d2a37ad5f6eb3087
KangSeockChan <ksc7299@naver.com>
2017-06-20 21:28:42 +0900

    Kang : 폴더 정리

---
+++ Assets/KangSeokChan/Scripts/BoxColliderMng.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxColliderMng : MonoBehaviour {
+
+    public GameObject BoxAlpha;
+    public GameObject Box;
+
+
+	
+	//// Update is called once per frame
+	//void Update () {
+		
+
+
+	//}
+}
---
+++ Assets/KangSeokChan/Scripts/ResetPosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour {
+
+    public float Posx;
+    public float Posy;
+    public float Posz;
+
+
+    public void ResetPos()
+    {
+        gameObject.transform.position = new Vector3(Posx,Posy,Posz);
+        gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        gameObject.GetComponent<BoxCollider>().enabled = true;
+    }
+}
